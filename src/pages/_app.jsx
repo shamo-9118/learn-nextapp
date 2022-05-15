b79bf16a7a8463a473f15c6f2f330d2b795aed8d
shamo-9118 <won9118391183@gmail.com>
2022-05-16 08:27:09 +0900

    ♻️ eslintの設定＆勉強

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
+import {useBgLightBlue} from "../hooks/useBgLightBlue"
 
 function MyApp({ Component, pageProps }) {
+  useBgLightBlue();
   return <Component {...pageProps} />
 }
 
