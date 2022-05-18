232261592d9d98ee7131d618514c3054304f17c5
shamo-9118 <won9118391183@gmail.com>
2022-05-18 21:13:34 +0900

    ♻️ リファクタリング

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "../../../styles/Home.module.css";
-import { useRouter } from "next/router";
 import { Header } from "../../../components/Header";
 import { CommentComponente } from "../../../components/Comment";
 
