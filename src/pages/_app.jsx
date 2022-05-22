f7ed90d6de311d57cb3ee3711f9d97c47a3d0d21
shamo-9118 <won9118391183@gmail.com>
2022-05-22 16:41:18 +0900

    ♻️ fetcherをグローバルに定義

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,26 @@
 import classes from "../styles/globals.css";
 import { Layout } from "../components/Layouts/Layout";
+import { SWRConfig } from "swr";
+SWRConfig
+
+export const fetcher = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("エラーが発生したため、データ取得できませんでした");
+  }
+
+  const json = await response.json();
+  return json;
+};
 
 function MyApp({ Component, pageProps }) {
   return (
+    <SWRConfig value = {{fetcher}}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </SWRConfig>
   );
 }
 
--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,10 +1,10 @@
-export const fetcher = async (url) => {
-  const response = await fetch(url);
+// export const fetcher = async (url) => {
+//   const response = await fetch(url);
 
-  if (!response.ok) {
-    throw new Error("エラーが発生したため、データ取得できませんでした");
-  }
+//   if (!response.ok) {
+//     throw new Error("エラーが発生したため、データ取得できませんでした");
+//   }
 
-  const json = await response.json();
-  return json;
-};
+//   const json = await response.json();
+//   return json;
+// };
