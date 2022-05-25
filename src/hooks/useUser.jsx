9d8d1786aadecef0bc2ba8885b7cee352be905a3
shamo-9118 <won9118391183@gmail.com>
2022-05-25 10:33:43 +0900

    ✅ イミュータブルを使って開発をしてみた

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,10 +1,10 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 import { fetcher } from "../utils/fetcher";
 
 export const useUser = () => {
   const router = useRouter();
-  const { data, error } = useSWR(
+  const { data, error } = useSWRImmutable(
     router.query.id
       ? `https://jsonplaceholder.typicode.com/users/${router.query.id}`
       : null,
